1d027c66294efe4d74c9785fab602e36d0e0fdfa
Clement <perso@clementhurel.fr>
2022-02-25 21:18:57 +0900

    New project

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const RPS = ['Rock', 'Paper', 'Scissors'];
+let playerSelection = prompt('Please choose between Rock, Paper and Scissors:')
+let computerSelection = computerPlay()
+
+function letItPlay() {
+    playerSelection = playerSelection.toLowerCase();
+
+}
+
+if ()
+
 
 function computerPlay() {
        return RPS[Math.floor(Math.random() * RPS.length)] 
     }
    
-    console.log(computerPlay());
+    console.log(computerSelection);
 
