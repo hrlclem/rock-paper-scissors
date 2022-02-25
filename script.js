dc028ba673658647f3c639be77ac5081057ac915
Clement <perso@clementhurel.fr>
2022-02-25 21:12:07 +0900

    New project

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+const RPS = ['Rock', 'Paper', 'Scissors'];
+
+function computerPlay() {
+       return RPS[Math.floor(Math.random() * RPS.length)] 
+    }
+   
+    console.log(computerPlay());
+
